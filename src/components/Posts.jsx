1c598f5caa1df4d5e3049015c9f7da332add34f3
abrorb3k <abrorb3k0219@gmail.com>
2025-03-13 10:53:05 +0500

    do

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,59 +1,3 @@
-// import React from "react";
-// import { useDispatch, useSelector } from "react-redux";
-// import PostCard from "./PostCard";
-// import { setSelectedPost } from "../redux/postSlice";
-
-// function Posts() {
-//   const { posts, isLoading, error } = useSelector((state) => state.posts);
-//   const dispatch = useDispatch()
-//   if (isLoading) {
-//     return (
-//       <div className="text-center text-4xl mt-10">
-//         <i className="fa fa-circle-notch fa-spin"></i>
-//       </div>
-//     );
-//   }
-//   if (error) {
-//     return (
-//       <div className="text-center text-xl mt-10 flex justify-center items-center gap-2">
-//         <i className="fa fa-circle-exclamation text-red-300 text-2xl"></i>
-//         <p className="text-3xl text-gray-300">
-//           Something went wrong! Please try later.
-//         </p>
-//       </div>
-//     );
-//   }
-//   return (
-//     <div className="">
-//       <div className="grid grid-cols-6 gap-7 mt-9 ">
-//         {posts && posts.map((p) => <PostCard key={p.id} post={p} />)}
-//       </div>
-//       {/* modal */}
-//       {setSelectedPost != null && (
-//         <div className="fixed user-modal top-0 left-0 right-0 bottom-0 z-10 bg-black/60 flex justify-center items-center">
-//           <button onClick={() => dispatch(setSelectedPost(null))} className="absolute top-3 text-white right-5 cursor-pointer">
-//             <i className="fa fa-close fa-2x"></i>
-//           </button>
-//           <div className="rounded-2xl bg-white">
-//             {isLoading ? (
-//               <div className="w-[100px] aspect-square text-gray-400 flex items-center justify-center">
-//               <i className="fa fa-circle-notch fa-spin"></i>
-//             </div>
-//             ) : 
-//             <PostCard post={{title:"hjcvjdvjh", id:posts.id}}/>
-//             }
-            
-//           </div>
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default Posts;
-
-
-
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import PostCard from "./PostCard";
@@ -95,6 +39,7 @@ function Posts() {
       </div>
 
       {/* Modal */}
+      
       {selectedPost && (
         <div className="fixed top-0 left-0 right-0 bottom-0 z-10 bg-black/60 flex justify-center items-center">
           <button
